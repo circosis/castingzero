aca75414905f3ebb8e1320dfc4306686ffa6ee18
unknown <juanmcirco@gmail.com>
2013-04-21 21:16:21 -0300

    se le saco el 960grid

---
+++ application/controllers/ingresoController.php
@@ -0,0 +1,40 @@
+<?php
+class IngresoController extends Zend_Controller_Action{
+    public function init()
+    {
+    }
+    public function indexAction()
+    {
+
+		$formulario = new Application_Form_AspirantesForm();
+		$this->view->form = $formulario;
+		if ($this->_request->isPost())
+		{
+			$formData = $this->_request->getPost();
+			if ($formulario->isValid($formData))
+			{
+				
+				
+			$name = $formulario->getValue('name');
+			$surname = $formulario->getValue('surname');
+			$telefono = $formulario->getValue('telefono');
+			$cel = $formulario->getValue('cel');
+			$PIN = $formulario->getValue('PIN');
+			$mail = $formulario->getValue('email');
+			$multiselect1 = $formulario->getValue('multiselect1');
+			$Sexo = $formulario->getValue('Sexo');
+			$fechaNacimiento = $formulario->getValue('fechaNacimiento');
+			$foto1 = $formulario->getValue('foto1');
+			$foto2 = $formulario->getValue('foto2');
+			$foto3 = $formulario->getValue('foto3');
+			
+			$register = new Application_Model_Aspirantes();
+			$register -> add($name, $surname, $telefono, $cel, $PIN, $mail, $multiselect1, $Sexo, $foto1, $foto2, $foto3);
+			$this->view->form = 'Tus datos fueron enviados correctamente. Casting Zero los avaluará.'; 
+			}
+		}
+	}
+}
+
+
+
