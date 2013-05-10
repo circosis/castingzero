0fe9872db954d4a7a413b11a07bfe5057ab44346
unknown <juanmcirco@gmail.com>
2013-05-10 08:48:09 -0300

    se modifico la ruta public por htdocs

--- application/models/Aspirantes.php
+++ application/models/Aspirantes.php
@@ -10,14 +10,12 @@ class Application_Model_Aspirantes
 	
 	}
 	public function view()
-    {
- 
-    $select = $this -> _dbTableAspirantes -> select();
-        $select->from($this -> _dbTableAspirantes,
-                        array('id','nombre', 'apellido','telefono', 'cel', 'PIN', 'email', 'multiselect1','multiselect2','Sexo', 'fechaNac'));
- 
-         return $this -> _dbTableAspirantes -> fetchAll($select);
-    }
+        {
+        $select = $this -> _dbTableAspirantes -> select();
+            $select->from($this -> _dbTableAspirantes,array('id','nombre', 'apellido','telefono', 'cel', 'PIN', 'email', 'multiselect1','multiselect2','Sexo', 'fechaNac','foto1','foto2','foto3'));
+
+             return $this -> _dbTableAspirantes -> fetchAll($select);
+        }
 	public function add($name, $surname, $telefono, $cel, $PIN, $mail, $multiselect1,$multiselect2, $Sexo, $fechaNacimiento, $foto1, $foto2, $foto3)
 	{
 		$data = array ('pendiente' => 'false','nombre' => $name , 'apellido' => $surname,'telefono' => $telefono,'cel' => $cel,'PIN' => $PIN,'email' => $mail,'multiselect1' => $multiselect1,'multiselect2' => $multiselect2,'Sexo' => $Sexo,'fechaNac' => $fechaNacimiento,'foto1' => $foto1,'foto2' => $foto2,'foto3' => $foto3);
